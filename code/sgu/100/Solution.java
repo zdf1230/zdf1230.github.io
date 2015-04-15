9a702ccb822e7ab8ef5dd245dc4b92e24d7932e9
Zhao Dongfang <615328619@qq.com>
2015-04-15 23:54:11 +0800

    UpdateSGU

---
+++ code/sgu/100/Solution.java
@@ -0,0 +1,15 @@
+// SGU 100 -- A+B
+import java.util.Scanner;
+
+
+public class Solution {
+
+	public static void main(String[] args) {
+		Scanner conin = new Scanner(System.in);
+		int a = conin.nextInt();
+		int b = conin.nextInt();
+		conin.close();
+		System.out.println(a + b);
+	}
+
+}
